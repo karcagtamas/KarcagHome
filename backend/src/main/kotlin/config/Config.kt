cd700734fefb25cc4c0690a178e38b5038185701
Karcag Tamas <karcagtamas@outlook.com>
2026-03-28 22:19:34 +0100

    #54 Add enhanced environment config

--- backend/src/main/kotlin/config/Config.kt
+++ backend/src/main/kotlin/config/Config.kt
@@ -1,9 +1,22 @@
 package config
 
+import com.typesafe.config.ConfigFactory
 import io.ktor.server.application.Application
+import io.ktor.server.config.ApplicationConfig
+import io.ktor.server.config.HoconApplicationConfig
+import io.ktor.server.config.withFallback
+import org.koin.ktor.ext.get
+
+fun Application.getConfig(): ApplicationConfig {
+    val env = environment.config.propertyOrNull("ktor.environment")?.getString()
+        ?: System.getenv("KTOR_ENV")
+        ?: "dev"
+
+    return HoconApplicationConfig(ConfigFactory.load("application-$env.conf")).withFallback(environment.config)
+}
 
 fun Application.loadDatabaseConfig(): DatabaseConfig {
-    val config = environment.config.config("database")
+    val config = get<ApplicationConfig>().config("database")
 
     return DatabaseConfig(
         driver = config.property("driver").getString(),
--- backend/src/main/kotlin/plugins/DI.kt
+++ backend/src/main/kotlin/plugins/DI.kt
@@ -1,13 +1,16 @@
 package plugins
 
+import config.getConfig
 import config.loadDatabaseConfig
 import database.DatabaseFactory
 import io.ktor.server.application.*
+import io.ktor.server.config.ApplicationConfig
 import org.koin.dsl.module
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
 val appModule = module {
+    single<ApplicationConfig> { get<Application>().getConfig() }
     single { get<Application>().loadDatabaseConfig() }
 
     single { DatabaseFactory.init(get()) }

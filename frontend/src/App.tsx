7f0f24d1830f1df3f372aed47a89ae7587d34f45
Karcag Tamas <karcagtamas@outlook.com>
2026-04-29 13:49:12 +0200

    #64 Add theme selector

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,22 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
-import { FluentProvider, makeStyles, webLightTheme } from '@fluentui/react-components';
+import {
+  Dropdown,
+  Option,
+  FluentProvider,
+  makeStyles,
+  tokens,
+  webDarkTheme,
+  webLightTheme,
+} from '@fluentui/react-components';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ExpensesPage } from './modules/expenses/pages/ExpensesPage';
 import { CurrenciesPage } from './modules/expenses/pages/CurrenciesPage';
 import { MeasurementPage } from './modules/measurements/pages/MeasurementPage';
 import { AppBar } from './components/common/AppBar';
+import { useState } from 'react';
+
+type ThemeMode = 'light' | 'dark';
 
 const useStyles = makeStyles({
   provider: {
@@ -17,23 +28,39 @@ const useStyles = makeStyles({
     display: 'flex',
     flex: 1,
     flexDirection: 'column',
-  },
-  content: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'column',
+    backgroundColor: tokens.colorNeutralBackground1,
+    color: tokens.colorNeutralForeground1,
   },
 });
 
 function App() {
   const styles = useStyles();
 
+  const [themeMode, setThemeMode] = useState<ThemeMode>('light');
+
+  const theme = themeMode === 'dark' ? webDarkTheme : webLightTheme;
+
   return (
-    <FluentProvider className={styles.provider} theme={webLightTheme}>
+    <FluentProvider className={styles.provider} theme={theme}>
       <QueryClientProvider client={new QueryClient()}>
         <BrowserRouter>
           <div className={styles.frame}>
-            <AppBar title="KarcagHome"></AppBar>
+            <AppBar
+              title="KarcagHome"
+              route="/"
+              right={
+                <Dropdown
+                  value={themeMode === 'dark' ? 'Dark' : 'Light'}
+                  selectedOptions={[themeMode]}
+                  onOptionSelect={(_, data) => {
+                    setThemeMode(data.optionValue as ThemeMode);
+                  }}
+                >
+                  <Option value="light">Light</Option>
+                  <Option value="dark">Dark</Option>
+                </Dropdown>
+              }
+            ></AppBar>
             <Routes>
               <Route path="/" element={<HomePage />} />
               <Route path="/dashboard" element={<MeasurementPage />} />
--- frontend/src/components/common/AppBar.tsx
+++ frontend/src/components/common/AppBar.tsx
@@ -1,49 +1,70 @@
 import { makeStyles, tokens } from '@fluentui/react-components';
+import { useNavigate } from 'react-router-dom';
 
 const useStyles = makeStyles({
   root: {
     height: '48px',
     display: 'flex',
+    flexDirection: 'row',
+    width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
 
     backgroundColor: tokens.colorNeutralBackground1,
     borderBottom: `1px solid ${tokens.colorNeutralStroke2}`,
-    padding: `0 ${tokens.spacingHorizontalM}`,
   },
 
   left: {
     display: 'flex',
+    flex: 1,
     alignItems: 'center',
-    gap: tokens.spacingHorizontalM,
+    justifyContent: 'left',
+    gap: tokens.spacingHorizontalS,
+    height: '48px',
+    padding: `0 ${tokens.spacingHorizontalM}`,
   },
 
   title: {
+    display: 'flex',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '48px',
+    padding: `0 ${tokens.spacingHorizontalM}`,
     fontSize: tokens.fontSizeBase500,
     fontWeight: tokens.fontWeightSemibold,
+    ':hover': {
+      cursor: 'pointer',
+    },
   },
 
   right: {
     display: 'flex',
+    flex: 1,
     alignItems: 'center',
+    justifyContent: 'right',
+    padding: `0 ${tokens.spacingHorizontalM}`,
     gap: tokens.spacingHorizontalS,
+    height: '48px',
   },
 });
 
 type Props = {
   title: string;
+  route: string;
   left?: React.ReactNode;
   right?: React.ReactNode;
 };
 
-export const AppBar: React.FC<Props> = ({ title, left, right }) => {
+export const AppBar: React.FC<Props> = ({ title, route, left, right }) => {
   const styles = useStyles();
+  const navigate = useNavigate();
 
   return (
     <div className={styles.root}>
-      <div className={styles.left}>
-        {left}
-        <div className={styles.title}>{title}</div>
+      <div className={styles.left}>{left}</div>
+      <div className={styles.title} onClick={() => navigate(route)}>
+        {title}
       </div>
       <div className={styles.right}>{right}</div>
     </div>
